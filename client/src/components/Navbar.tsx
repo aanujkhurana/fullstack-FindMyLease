c52a91d7d1bcab89064b646e8e90d41fe7a408cf
aanujkhurana <aanujkhurana@gmail.com>
2025-03-23 20:43:35 +1000

    fix tailwind error

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -52,15 +52,15 @@ const Navbar = () => {
             <div className="flex items-center gap-3">
               <Image
                 src="/logo.svg"
-                alt="Rentiful Logo"
+                alt="Find My Lease Logo"
                 width={24}
                 height={24}
                 className="w-6 h-6"
               />
               <div className="text-xl font-bold">
-                RENT
+                Find
                 <span className="text-secondary-500 font-light hover:!text-primary-300">
-                  IFUL
+                  MyLease
                 </span>
               </div>
             </div>
